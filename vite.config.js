7f4a67495f70856efff005c827610dbb5334da0f
ronx-labs <ron.sam624@gmail.com>
2024-10-27 20:25:48 -0400

    fix: vite config issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
 
 export default defineConfig({
+  build: {
+    target: "es2020",
+  },
   plugins: [
     react(),
-    crx({ manifest }),
+    crx({ manifest })
   ],
-})
+  optimizeDeps: {
+    exclude: ["koffi", "re2"]
+  },
+});
